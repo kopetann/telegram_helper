d274fb15ff4edb07da4f323313556149a3b1be24
Komov Nikita <komov@qtim.ru>
2023-03-08 19:21:16 +0300

    master: Installed required packages and updated config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Config } from './common/config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => new Config().getTypeOrmConfig(),
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/common/config.ts
+++ src/common/config.ts
@@ -1,4 +1,5 @@
 import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export class Config {
   private readonly configService: ConfigService;
@@ -7,7 +8,7 @@ export class Config {
     this.configService = new ConfigService();
   }
 
-  static get swagger() {
+  static get swaggerConfig() {
     return {
       title: 'telegram_helper',
       description: 'The telegram_helper API description',
@@ -19,11 +20,26 @@ export class Config {
     };
   }
 
+  public getTypeOrmConfig(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: 'postgres',
+      port: this.get('DB_PORT', 5432),
+      username: this.get('DB_LOGIN'),
+      password: this.get('DB_PASSWORD'),
+      database: this.get('DB_NAME'),
+      entities: ['src/**/*.entity{.ts,.js}'],
+      migrations: ['src/migration/*{.ts,.js}'],
+      migrationsTableName: 'telegram_manager_migrations',
+      migrationsRun: false,
+    };
+  }
+
   public getConfig(): Config {
     return this;
   }
 
-  public get<T = any>(propertyKey: string, defaultProperty: T): any {
-    this.configService.get(propertyKey, defaultProperty);
+  public get<T = any>(propertyKey: string, defaultProperty?: T): T | undefined {
+    return this.configService.get(propertyKey, defaultProperty);
   }
 }
