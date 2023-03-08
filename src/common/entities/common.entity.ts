74ff8e999ef5493abcee28179c050b767f8a05e0
Komov Nikita <komov@qtim.ru>
2023-03-08 19:21:59 +0300

    master: Common entity

---
+++ src/common/entities/common.entity.ts
@@ -0,0 +1,20 @@
+import {
+  CreateDateColumn,
+  DeleteDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+export abstract class CommonEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
