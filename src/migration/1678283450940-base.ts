94f1dbdd4577bf875f01394611ca51ee9ccf4192
Komov Nikita <komov@qtim.ru>
2023-03-23 00:31:44 +0300

    master: Removed useless files

--- src/migration/1678283450940-base.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-
-export class base1678283450940 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
--- src/migration/1678303130937-base.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-
-export class base1678303130937 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
