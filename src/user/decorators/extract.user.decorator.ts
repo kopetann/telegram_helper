62630fd74053edd2cf0107fdb7442c7c45c33ef5
Komov Nikita <komov@qtim.ru>
2023-03-23 23:32:51 +0300

    master: Removed log from decorator

--- src/user/decorators/extract.user.decorator.ts
+++ src/user/decorators/extract.user.decorator.ts
@@ -1,8 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const ExtractUser = createParamDecorator(
-  (data: unknown, ctx: ExecutionContext) => {
-    console.log(data);
+  (_: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
     return request.user;
   },
